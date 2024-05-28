b295e499f61ab93d98355d754a03e2829d6e8349
LumiHunter <hwangje36@gmail.com>
2024-05-28 22:38:53 +0900

    0528 중간저장

---
+++ Level.1/l01-ch02-01.py
@@ -0,0 +1,12 @@
+# print separator 옵션 사용
+print('P', 'Y', 'T', 'H','O','N')    # default: ' '
+print('010', '7777', '7777', sep='-')
+print('python', 'google.com', sep='@')
+print()
+
+# print end 옵션 사용
+print('Welcome To', end=' ')
+print('IT News', end=' ')
+print('Web Site')    # default: \n
+
+print()
