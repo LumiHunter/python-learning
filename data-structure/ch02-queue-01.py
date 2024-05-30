5b3bc3a7df9ac02f5f7174bd0945299fe4eae8e0
LumiHunter <hwangje36@gmail.com>
2024-05-30 23:54:53 +0900

    queue: isEmpty, isFull 구현

--- data-structure/ch02-queue-01.py
+++ data-structure/ch02-queue-01.py
@@ -4,3 +4,9 @@ class ArrayQueue():
         self.array = [None] * capacity
         self.front = 0
         self.rear = 0
+        
+    def isEmpty(self):
+        return self.front == self.rear
+    
+    def isFull(self):
+        return self.front == (self.rear + 1) % self.capacity
