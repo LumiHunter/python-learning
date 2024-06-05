592ca4a4d0a9b5c5ee49ab36a443d99697b660ed
LumiHunter <hwangje36@gmail.com>
2024-06-05 23:35:34 +0900

    0605 중간저장

---
+++ Level.1/l01-ch04-02.py
@@ -0,0 +1,11 @@
+# for in <collection>
+#   <loop body>
+
+for v1 in range(10):
+    print('v1 is ', v1)
+    
+for v2 in range(1, 11):
+    print('v2 is ', v2)
+    
+for v3 in range(1, 11, 2):
+    print('v3 is ', v3)
