80006b0a1946f16be91c09a76b27e0d61de45180
Jieun Hwang <hwangje36@gmail.com>
2024-06-14 14:38:41 +0900

    L01C09 완

--- Level.1/l01-ch09-02.py
+++ Level.1/l01-ch09-02.py
@@ -2,34 +2,34 @@
 
 import csv
 
-# with open('./Level.1/resource/test1.csv', 'r') as f:
-#     reader = csv.reader(f)
-#     next(reader)    # 커서 하나 넘김: 헤더 스킵 -> pop() 처럼 return 하므로, 하나씩 출력할 때 사용하기도 함.
-#     print(reader)
-#     print(type(reader))
-#     print(dir(reader))
+with open('./Level.1/resource/test1.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)    # 커서 하나 넘김: 헤더 스킵 -> pop() 처럼 return 하므로, 하나씩 출력할 때 사용하기도 함.
+    print(reader)
+    print(type(reader))
+    print(dir(reader))
 
-#     for c in reader:
-#         print(c)    # 리스트
-#         print(': '.join(c))    # 문자열 함수 -> ch03-02 참조: ' ' 앞, 뒤에 문자 추가
+    for c in reader:
+        print(c)    # 리스트
+        print(': '.join(c))    # 문자열 함수 -> ch03-02 참조: ' ' 앞, 뒤에 문자 추가
 
-# with open('./Level.1/resource/test2.csv', 'r') as f:
-#     reader = csv.reader(f, delimiter='|')    # 지정된 분할 문자(default는 ,)
-#     for c in reader:
-#         print(c)
+with open('./Level.1/resource/test2.csv', 'r') as f:
+    reader = csv.reader(f, delimiter='|')    # 지정된 분할 문자(default는 ,)
+    for c in reader:
+        print(c)
 
-# with open('./Level.1/resource/test1.csv', 'r') as f:
-#     reader = csv.DictReader(f)
-#     print(reader)
-#     print(type(reader))
-#     print(dir(reader))
+with open('./Level.1/resource/test1.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    print(reader)
+    print(type(reader))
+    print(dir(reader))
 
-#     for c in reader:
-#         print(c)    # 자동으로 header가 key가 됨!
-    # for c in reader:
-    #     for k, v in c.items():
-    #         print(k, v)
-    #     print('--------------')
+    for c in reader:
+        print(c)    # 자동으로 header가 key가 됨!
+    for c in reader:
+        for k, v in c.items():
+            print(k, v)
+        print('--------------')
 
 w = [[1,2,3], [4,5,6], [7,8,9],[10,11,12],[13,14,15],[16,17,18],[19,20,21]]
 
