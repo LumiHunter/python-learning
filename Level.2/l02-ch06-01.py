36606981b66dfb562a79a5ec18b27cc7b4f66ceb
Jieun Hwang <hwangje36@gmail.com>
2024-06-27 16:07:48 +0900

    L02C06-01

--- Level.2/l02-ch06-01.py
+++ Level.2/l02-ch06-01.py
@@ -29,8 +29,62 @@ from collections import abc    # abc: abstract class의 약자.
 print(isinstance(t, abc.Iterable))    # 추상클래스-Iterable을 상속받은 객체가 맞는지 확인.
 print()
 
-# next
+# next 패턴
 class WordSplitter():
     def __init__(self, text):
         self._idx = 0
         self._text = text.split(' ')
+
+    def __next__(self):
+        print('Called __next__')
+        try:
+            word = self._text[self._idx]
+        except IndexError:
+            raise StopIteration('Stopped Iteration.')
+        self._idx += 1
+        return word
+    
+    def __repr__(self):
+        return 'WordSplit(%s)' % (self._text)
+    
+wi = WordSplitter('Do today what you could do tommorrow')
+print(wi)
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+# print(next(wi))
+print()
+
+# Generator 패턴
+# 1. 지능형 리스트, 딕셔너리, 집합 -> 데이터 양 증가하면 메모리 사용량 증가 -> 제네레이터 사용을 권장함
+# 2. 단위 실행 가능한 코루틴(Corotine) 구현과 연동
+# 3. 작은 메모리 조각 사용
+
+class WordSplitGenerator():
+    def __init__(self, text):
+        self._text = text.split(' ')
+
+    def __iter__(self):
+        print('Called __iter__')
+        for word in self._text:
+            yield word     # 제네레이터!! 인덱스를 명시하지 않아도 내부적으로 다음에 반환될 값에 대한 상태를 기억하고 있다!
+    
+    def __repr__(self):
+        return 'WordSplit(%s)' % (self._text)
+
+wg = WordSplitGenerator('Do today what you could do tommorrow')
+wt = iter(wg)
+print(wg)
+print(wt)
+print(next(wt))
+print(next(wt))    # yield 키워드에서 나가지 않기 때문에, 'Called __iter__'는 다시 출력되지 않는다!
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+# print(next(wt))    # StopIteration 예외 발생
