33469aa32306e838941cb1c74db9b9c16081bf71
LumiHunter <hwangje36@gmail.com>
2024-07-27 15:04:34 +0900

    L03C01-01 수정

--- Level.3/l03-ch01-01.py
+++ Level.3/l03-ch01-01.py
@@ -22,3 +22,25 @@ def foobar():
     except UnboundLocalError:
         print("local variable 'c' referenced before assignment")
 foobar()
+
+d = 50
+def barfoo():
+    global d    # global 변수를 local scope에서 수정할 수 있으려면, global 키워드를 쓴다
+    d = 60
+    d += 100    # 그러나 전역변수는 '변하지 않는 변수'라는 전제가 있기 때문에, 권장되지는 않는다.
+    print(d)
+
+barfoo()
+
+def outer():
+    e = 70
+    def inner():
+        # UnboundLocall Error 상황: inner는 outer의 변수를 수정할 수 없음
+        # e += 10
+        nonlocal e    # inner의 변수를 받아 수정 가능할 수 있도록 하는 nonlocal 키워드
+        e += 10
+        print(e)
+    return inner
+
+in_test = outer()    # Closure
+in_test()
