85be24428518a5f68e87b5e7db777e9e7aa74ad0
LumiHunter <hwangje36@gmail.com>
2025-11-19 00:35:32 +0900

    IO bound 완료

--- Level.4/l04-ch03-05-4.py
+++ Level.4/l04-ch03-05-4.py
@@ -3,35 +3,28 @@ Section 3
 Concurrency, CPU Bound vs I/O bound - I/O bound(2) - threading vs asyncio vs multiprocessing
 Keyword - I/O bound, requests, threading
 """
-
-import multiprocessing
 import aiohttp, time, asyncio
 
 # ===== I/O bound asyncio 예제 =====
-# threading보다 높은 복잡도
+# threading보다 높은 복잡도 -> async, await, 비동기식으로 작동하는 패키지 필요.
 
-# 각 프로세스 메모리 영역에 생성되는 객체(독립적)
-# 함수 실행 할 때 마다 객체 생성은 좋지 않음 -> 미리 독립적으로 만들어놓은 다음 각 프로세스마다 할당
-session = None 
-
-def set_global_session():
-    global session
-    if not session:
-        session = requests.Session()
-
-def request_site(url):
+async def request_site(session, url):
     print(session)
     print(session.headers)
-    
-    with session.get(url) as response:
-        name = multiprocessing.current_process().name
-        print(f'{name} -> Read Contents: {len(response.content)}, Status Code: {response.status_code} from {url}')
 
-def request_all_sites(urls):
-    # 멀티프로세싱 실행
-    # 반드시 processes 개수 조절 후 session 객체 및 실행 시간 확인
-    with multiprocessing.Pool(initializer=set_global_session, processes=4) as pool:
-        pool.map(request_site, urls)
+    async with session.get(url) as response:
+        print('Read Contents {0}, from {1}'.format(response.content_length, url))
+
+async def request_all_sites(urls):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for url in urls:
+            task = asyncio.ensure_future(request_site(session, url))
+            tasks.append(task)
+
+        print(*tasks)
+        print(tasks)
+        await asyncio.gather(*tasks, return_exceptions=True)
 
 def main():
     urls = [
